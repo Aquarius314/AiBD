42d60ea35b9741c566870a46febea14f85769685
jakubwojcik <jakubwojcik.post@gmail.com>
2019-10-15 23:55:00 +0200

    adding quotes for conversion from sav to csv

--- lab1/Command-Files/convert.r
+++ lab1/Command-Files/convert.r
@@ -9,5 +9,5 @@ print("R script: converting .sav to .csv")
 
 library(foreign)
 
-write.table(read.spss("Original-Data/pew.sav"), file="Analysis-Data/converted.csv", quote = FALSE, sep = ',')
+write.table(read.spss("Original-Data/pew.sav"), file="Analysis-Data/converted.csv", quote = TRUE, sep = ',')
 
